f643e266743f147caa2f66186398469d298b6a23
Armin Kazemi <arminkz@live.com>
2019-07-23 00:29:22 +0430

    login page completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  login_subpage = 1;
 
   constructor() { }
 
