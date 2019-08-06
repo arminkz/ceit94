20d4075553d5725248dbac2c26bd6831c54b237b
Armin Kazemi <arminkz@live.com>
2019-08-06 20:29:12 +0430

    Landing Page Responsive

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, OnDestroy {
+
+  interval: any;
+  now: Date;
+  grad_rem = 0;
+
+  grad_hour = 0;
+  grad_min = 0;
+  grad_sec = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.interval = setInterval(() => {
+      this.now = new Date();
+      this.grad_rem = new Date(2019, 11, 6).getTime() - this.now.getTime();
+
+      this.grad_hour = Math.floor(this.grad_rem / (1000 * 3600));
+      this.grad_min = Math.floor(this.grad_rem / (1000 * 60)) - this.grad_hour * 60;
+      this.grad_sec = Math.floor(this.grad_rem / 1000) - this.grad_hour * 3600 - this.grad_min * 60;
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
   }
 
 }
