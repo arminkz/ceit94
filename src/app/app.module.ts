e675c6d159b81cc19da6b873bd90cd6d2e61d9be
Armin Kazemi <arminkz@live.com>
2019-08-07 01:00:05 +0430

    added search for students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import {Form, FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EditprofileComponent } from './editprofile/editprofile.component';
+import { FilterPipe } from './shared/pipes/filter.pipe';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { EditprofileComponent } from './editprofile/editprofile.component';
     LandingComponent,
     LoginComponent,
     ProfileComponent,
-    EditprofileComponent
+    EditprofileComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Student} from '../shared/models/student.model';
 import {StudentService} from '../student.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-students',
@@ -10,11 +11,19 @@ import {StudentService} from '../student.service';
 export class StudentsComponent implements OnInit {
 
   students: Student[];
+  searchText: string;
 
-  constructor(private studentService: StudentService) { }
+  constructor(
+    private studentService: StudentService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
     this.studentService.getStudents().subscribe(students => this.students = students);
+  }
+
+  clickedStudent(student: Student) {
+    this.router.navigateByUrl('/' + student.username);
   }
 
 }
